b8be6cb6244f1ed9dea5dab8eb63a89cc47341d5
Leon Windhorst <leon.windhorst@web.de>
2022-10-29 20:21:25 +0200

    Update LSP keymaps

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,30 +1,32 @@
 require('lsp-format').setup({})
 
 local opts = { noremap = true, silent = true }
-vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
-vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
+local map = vim.keymap.set
+
+map('n', '<space>e', vim.diagnostic.open_float, opts)
+map('n', '[d', vim.diagnostic.goto_prev, opts)
+map('n', ']d', vim.diagnostic.goto_next, opts)
+map('n', '<space>q', vim.diagnostic.setloclist, opts)
 
 local on_attach = function(client, bufnr)
     require('lsp-format').on_attach(client)
 
     local bufopts = { noremap = true, silent = true, buffer = bufnr }
-    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
-    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
-    vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
-    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
-    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
-    vim.keymap.set('n', '<Space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
-    vim.keymap.set('n', '<Space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
-    vim.keymap.set('n', '<Space>wl', function()
+    map('n', 'gD', vim.lsp.buf.declaration, bufopts)
+    map('n', 'gd', vim.lsp.buf.definition, bufopts)
+    map('n', 'K', vim.lsp.buf.hover, bufopts)
+    map('n', 'gi', vim.lsp.buf.implementation, bufopts)
+    map('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
+    map('n', '<Space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
+    map('n', '<Space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
+    map('n', '<Space>wl', function()
         print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
     end, bufopts)
-    vim.keymap.set('n', '<Space>D', vim.lsp.buf.type_definition, bufopts)
-    vim.keymap.set('n', '<Space>rn', vim.lsp.buf.rename, bufopts)
-    vim.keymap.set('n', '<Space>ca', vim.lsp.buf.code_action, bufopts)
-    vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
-    -- vim.keymap.set('n', '<Space>f', function() vim.lsp.buf.format { async = true } end, bufopts)
+    map('n', '<Space>D', vim.lsp.buf.type_definition, bufopts)
+    map('n', '<Space>rn', vim.lsp.buf.rename, bufopts)
+    map('n', '<Space>ca', vim.lsp.buf.code_action, bufopts)
+    map('n', 'gr', vim.lsp.buf.references, bufopts)
+    -- map('n', '<Space>f', function() vim.lsp.buf.format { async = true } end, bufopts)
 end
 
 local lsp_flags = {
