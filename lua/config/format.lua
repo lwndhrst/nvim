e0283e0d04b4440b0e1ba19a6038dc0c16864865
lwindhorst <lw@4so.de>
2022-11-29 15:55:50 +0100

    Set stylua format to use spaces instead of tabs

--- lua/config/format.lua
+++ lua/config/format.lua
@@ -1,6 +1,19 @@
 require("formatter").setup({
-    logging = true,
     filetype = {
-        rust = { require("formatter.filetypes.rust").rustfmt },
+        lua = {
+            function()
+                local stylua = require("formatter.filetypes.lua").stylua()
+                table.insert(stylua.args, 1, "--indent-type")
+                table.insert(stylua.args, 2, "Spaces")
+                return stylua
+            end,
+        },
+        rust = {
+            function()
+                local rustfmt = require("formatter.filetypes.rust").rustfmt()
+                rustfmt.args = { "--edition", "2021" }
+                return rustfmt
+            end,
+        },
     },
 })
