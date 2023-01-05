d2e4e9b29bc8be7a4de0be0d1be9a773772bcc2d
Leon Windhorst <leon.windhorst@web.de>
2023-01-05 15:04:51 +0100

    Add harpoon term keymap

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -28,7 +28,9 @@ function M.setup()
 
     -- harpoon
     local harpoon_mark = require("harpoon.mark")
+    local harpoon_term = require("harpoon.term")
     local harpoon_ui = require("harpoon.ui")
+    map("n", "<Space>jt", function() harpoon_term.gotoTerminal(1) end, opts) -- jump to term 1
     map("n", "<Space>ja", harpoon_mark.add_file, opts) -- add mark for current file
     map("n", "<Space>jl", harpoon_ui.toggle_quick_menu, opts) -- toggle menu
     map("n", "<C-j>", harpoon_ui.nav_next, opts) -- jump to next mark
