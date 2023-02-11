40a65b1715795b1adced0fd583a9a573ebd6277f
Leon Windhorst <leon.windhorst@web.de>
2023-02-11 10:49:33 +0100

    Update dashboard

--- lua/config/dashboard.lua
+++ lua/config/dashboard.lua
@@ -1,44 +1,54 @@
-local db = require("dashboard")
-
-db.custom_header = {
-    "                              ",
-    "                              ",
-    "                              ",
-    "                              ",
-    "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⠀⠀⠀⣰⣷⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
-    "⠀⠀⢀⣀⡀⠀⠀⠀⠀⠀⣸⣷⣄⠺⣿⣿⣿⠇⣼⣦⡀⠀⠀⠀⠀⣀⣠⡄⠀⠀",
-    "⠀⠀⠀⣿⣿⡷⢠⣾⣦⣀⠻⣿⣿⣷⢬⠛⣡⣾⠟⣉⣴⣾⣷⡄⢿⣿⡿⠀⠀⠀",
-    "⠀⠀⠀⢸⡿⢠⣿⣿⣿⣿⣷⡜⠟⣡⣾⣧⡙⠇⣼⣿⣿⣿⣿⣿⡄⢿⣷⠀⠀⠀",
-    "⠀⠀⠀⢾⡇⣼⣿⣿⣿⡿⢋⣴⣾⣿⣿⣿⣿⣦⡙⢿⣿⣿⣿⣿⣷⢸⣿⠀⠀⠀",
-    "⣤⣀⣀⣘⠇⢹⣿⡿⠿⢁⣿⣿⣿⣿⣿⣿⣿⣿⣿⣆⠹⣿⣿⣿⡏⠸⢋⣀⣀⣤",
-    "⢿⣿⣿⣿⣿⡌⢿⠃⣷⣤⣍⡛⠻⢿⣿⣿⣿⣿⣿⣿⣇⢹⣿⡟⢡⣿⣿⣿⣿⣿",
-    "⠘⣿⣿⣿⣿⣿⣦⢸⣿⣿⣿⣿⣿⣶⣤⣉⠻⣿⣿⣿⣿⠘⣡⣴⣿⣿⣿⣿⣿⠇",
-    "⠀⠘⢿⣿⣿⣿⣿⡘⣿⣿⣿⣿⣿⣿⣿⣿⣷⡌⢿⣿⡿⢸⣿⣿⣿⣿⣿⣿⠏⠀",
-    "⠀⠀⠀⠙⠿⣿⣿⣷⡘⢿⣿⣿⣿⣿⣿⣿⣿⡇⣸⣿⠃⣾⣿⣿⣿⣿⠟⠁⠀⠀",
-    "⠀⠀⠀⠀⠀⠈⠙⠛⠻⢶⡌⠙⠛⠿⠿⠛⣋⡴⠟⠡⠾⠛⠛⠋⠉⠀⠀⠀⠀⠀",
-    "                              ",
-    "                              ",
-}
-
-db.custom_center = {
-    {
-        icon = " ",
-        desc = "Find File   ",
-        action = "Telescope find_files",
+require("dashboard").setup({
+    theme = "doom",
+    config = {
+        header = {
+            "                              ",
+            "                              ",
+            "                              ",
+            "                              ",
+            "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⠀⠀⠀⣰⣷⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
+            "⠀⠀⢀⣀⡀⠀⠀⠀⠀⠀⣸⣷⣄⠺⣿⣿⣿⠇⣼⣦⡀⠀⠀⠀⠀⣀⣠⡄⠀⠀",
+            "⠀⠀⠀⣿⣿⡷⢠⣾⣦⣀⠻⣿⣿⣷⢬⠛⣡⣾⠟⣉⣴⣾⣷⡄⢿⣿⡿⠀⠀⠀",
+            "⠀⠀⠀⢸⡿⢠⣿⣿⣿⣿⣷⡜⠟⣡⣾⣧⡙⠇⣼⣿⣿⣿⣿⣿⡄⢿⣷⠀⠀⠀",
+            "⠀⠀⠀⢾⡇⣼⣿⣿⣿⡿⢋⣴⣾⣿⣿⣿⣿⣦⡙⢿⣿⣿⣿⣿⣷⢸⣿⠀⠀⠀",
+            "⣤⣀⣀⣘⠇⢹⣿⡿⠿⢁⣿⣿⣿⣿⣿⣿⣿⣿⣿⣆⠹⣿⣿⣿⡏⠸⢋⣀⣀⣤",
+            "⢿⣿⣿⣿⣿⡌⢿⠃⣷⣤⣍⡛⠻⢿⣿⣿⣿⣿⣿⣿⣇⢹⣿⡟⢡⣿⣿⣿⣿⣿",
+            "⠘⣿⣿⣿⣿⣿⣦⢸⣿⣿⣿⣿⣿⣶⣤⣉⠻⣿⣿⣿⣿⠘⣡⣴⣿⣿⣿⣿⣿⠇",
+            "⠀⠘⢿⣿⣿⣿⣿⡘⣿⣿⣿⣿⣿⣿⣿⣿⣷⡌⢿⣿⡿⢸⣿⣿⣿⣿⣿⣿⠏⠀",
+            "⠀⠀⠀⠙⠿⣿⣿⣷⡘⢿⣿⣿⣿⣿⣿⣿⣿⡇⣸⣿⠃⣾⣿⣿⣿⣿⠟⠁⠀⠀",
+            "⠀⠀⠀⠀⠀⠈⠙⠛⠻⢶⡌⠙⠛⠿⠿⠛⣋⡴⠟⠡⠾⠛⠛⠋⠉⠀⠀⠀⠀⠀",
+            "                              ",
+            "                              ",
+        },
+        center = {
+            {
+                icon = " ",
+                icon_hl = "@String",
+                desc = " Find File   ",
+                desc_hl = "@String",
+                action = "Telescope find_files",
+            },
+            {
+                icon = " ",
+                icon_hl = "@String",
+                desc = " Recent Files",
+                desc_hl = "@String",
+                action = "Telescope oldfiles",
+            },
+            {
+                icon = " ",
+                icon_hl = "@String",
+                desc = " Search Text ",
+                desc_hl = "@String",
+                action = "Telescope live_grep",
+            },
+            {
+                icon = " ",
+                icon_hl = "@String",
+                desc = " Config      ",
+                desc_hl = "@String",
+                action = "e ~/.config/nvim/init.lua",
+            },
+        },
     },
-    {
-        icon = " ",
-        desc = "Recent Files",
-        action = "Telescope oldfiles",
-    },
-    {
-        icon = " ",
-        desc = "Search Text ",
-        action = "Telescope live_grep",
-    },
-    {
-        icon = " ",
-        desc = "Config      ",
-        action = "e ~/.config/nvim/init.lua",
-    },
-}
+})
