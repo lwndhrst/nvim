16411ac3d2e0af6e0758152fc183c0f6c017f2d6
Leon Windhorst <leon.windhorst@web.de>
2023-02-12 10:37:35 +0100

    Add cmdline cmp

--- lua/config/cmp.lua
+++ lua/config/cmp.lua
@@ -27,6 +27,6 @@ cmp.setup.cmdline(":", {
     mapping = cmp.mapping.preset.cmdline(),
     sources = cmp.config.sources({
         { name = "path" },
-        -- { name = "cmdline" },
+        { name = "cmdline" },
     }),
 })
