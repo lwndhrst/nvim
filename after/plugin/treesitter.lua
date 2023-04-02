7fc1382aededb885c8f0027da443c8228ee2ed46
Leon Windhorst <leon@windhorst.dev>
2023-04-02 10:44:49 +0200

    update: set custom treesitter parser dir
    for installing parsers via TS commands on NixOS

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,3 +1,9 @@
+local parser_dir = vim.fn.stdpath("data") .. "/treesitter/parsers"
+vim.opt.runtimepath:append(parser_dir)
+
 require("nvim-treesitter.configs").setup({
+    parser_install_dir = parser_dir,
+    auto_install = true,
     highlight = { enable = true },
+    disable = { "latex" },
 })
