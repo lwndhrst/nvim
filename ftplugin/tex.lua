a770a7218fd4506525962c103758e5719aaaef28
Leon Windhorst <leon@windhorst.dev>
2023-05-22 13:53:35 +0200

    chore: improved umlaut replace function

--- ftplugin/tex.lua
+++ ftplugin/tex.lua
@@ -7,14 +7,14 @@ local map = vim.keymap.set
 
 -- replace latex notation with actual german umlaut and ß characters
 map("n", "<Space>ru", function()
-	vim.cmd(
-		[[!sed -i ]]
-			.. [[-e 's/\\"A/Ä/g ; s/\\"a/ä/g' ]]
-			.. [[-e 's/\\"O/Ö/g ; s/\\"o/ö/g' ]]
-			.. [[-e 's/\\"U/Ü/g ; s/\\"u/ü/g' ]]
-			.. [[-e 's/{\\ss}/ß/g ; s/\\ss /ß/g' ]]
-			.. vim.fn.expand("%:p") -- current buffer file path
-	)
+	vim.cmd([[s/\\"A/Ä/eg]])
+	vim.cmd([[s/\\"a/ä/eg]])
+	vim.cmd([[s/\\"O/Ö/eg]])
+	vim.cmd([[s/\\"o/ö/eg]])
+	vim.cmd([[s/\\"U/Ü/eg]])
+	vim.cmd([[s/\\"u/ü/eg]])
+	vim.cmd([[s/{\\ss}/ß/eg]])
+	vim.cmd([[s/\\ss /ß/eg]])
 end, opts)
 
 -- toggle german spell checking
