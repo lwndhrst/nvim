24b62b9cf6296a4847fd4f874dc6ab719c9cdad1
Leon Windhorst <leon@windhorst.dev>
2023-09-05 10:23:27 +0200

    refactor: cleanup lsp setup

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,53 +1,34 @@
 local keymaps = require("keymaps")
+local lsp = require("lspconfig")
 
 local on_attach = function(client, buf_nr)
 	keymaps.lsp_buf_maps(buf_nr)
 end
 
-local lsp = require("lspconfig")
-local capabilities = require("cmp_nvim_lsp").default_capabilities()
-local flags = {
-	debounce_text_changes = 150,
+-- default config
+local config = {
+	on_attach = on_attach,
+	capabilities = require("cmp_nvim_lsp").default_capabilities(),
+	flags = { debounce_text_changes = 150 },
 }
 
 -- c/c++
-lsp.clangd.setup({
-	on_attach = on_attach,
-	capabilities = capabilities,
-	flags = flags,
-})
+lsp.clangd.setup(config)
+
+-- glsl
+lsp.glslls.setup(config)
 
 -- nix
-lsp.nil_ls.setup({
-	on_attach = on_attach,
-	capabilities = capabilities,
-	flags = flags,
-})
+lsp.nil_ls.setup(config)
 
 -- odin
-lsp.ols.setup({
-	on_attach = on_attach,
-	capabilities = capabilities,
-	flags = flags,
-})
+lsp.ols.setup(config)
 
 -- rust
-lsp.rust_analyzer.setup({
-	on_attach = on_attach,
-	capabilities = capabilities,
-	flags = flags,
-})
+lsp.rust_analyzer.setup(config)
 
 -- tex
-lsp.texlab.setup({
-	on_attach = on_attach,
-	capabilities = capabilities,
-	flags = flags,
-})
+lsp.texlab.setup(config)
 
 -- zig
-lsp.zls.setup({
-	on_attach = on_attach,
-	capabilities = capabilities,
-	flags = flags,
-})
+lsp.zls.setup(config)
