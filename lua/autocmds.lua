0c18a4d97eb2d2ea0bf4af53b0d1fdec417d2fba
Leon Windhorst <leon@windhorst.dev>
2023-10-20 15:48:21 +0200

    chore: add autocmd for uni c++ style

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -6,3 +6,16 @@ autocmd("TermOpen", {
 		vim.wo.relativenumber = false
 	end,
 })
+
+-- only for computer graphics exercise
+autocmd({ "BufRead", "BufEnter" }, {
+	pattern = {
+		"/home/leon/Projects/cg1-exercise/**/*.cpp",
+		"/home/leon/Projects/cg1-exercise/**/*.h",
+	},
+	callback = function()
+		vim.bo.tabstop = 4
+		vim.bo.shiftwidth = 4
+		vim.bo.expandtab = false
+	end,
+})
